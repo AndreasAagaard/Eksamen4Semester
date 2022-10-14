e8c6ba35cbbd299954ba1d74233a54d49fe4b5ab
nicolaiskat <jegerrockstar@gmail.com>
2022-10-14 12:05:43 +0200

    small changes

--- item-solution/item-service/Program.cs
+++ item-solution/item-service/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddSingleton<ItemServiceService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
